5271b9b89129f6de210ad41f14d3d6ed4faf1dd2
monja119 <monja.sesame@gmail.com>
2023-11-09 22:10:47 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 function App() {
+  const [response, setResponse] = useState(null);
+  const [url, setUrl] = useState('');
+  const [method, setMethod] = useState('GET');
+  const [data , setData] = useState({});
+
+  const send = async () => {
+    await axios({
+      method,
+      url,
+      data,
+    })
+    .then(res => setResponse(res.data))
+    .catch(err => setResponse(err.message));
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input
+        type='text' 
+        name='url'
+        value={url}
+        style={{ marginTop: '20px' }}
+        onChange={e => setUrl(e.target.value)}
+      />
+      
+      <select
+        name='method'
+        value={method}
+        onChange={e => setMethod(e.target.value)}
+      >
+        <option value="GET">GET</option>
+        <option value="POST">POST</option>
+        <option value="PUT">PUT</option>
+        <option value="DELETE">DELETE</option>
+      </select>
+      <button onClick={send}>Send</button>
+      <br />
+      
+      {
+        (method === 'POST' || method === 'PUT') && (
+          <>
+            <textarea
+              name='data'
+              value={data}
+              onChange={e => setData(e.target.value)}
+            />
+          </>
+        )
+      }
+
+      {
+        response && (
+          <div>
+            <h2>Response :</h2>
+            <pre>{JSON.stringify(response, null, 2)}</pre>
+          </div>
+        )
+      }
+    </>
   );
 }
 
